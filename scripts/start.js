d4d21a7ad0281fbac496004403b9d00ae101f425
Dak Washbrook <vinlockz@gmail.com>
2019-09-30 02:51:20 -0700

    Added start script

---
+++ scripts/start.js
@@ -0,0 +1,12 @@
+require('@babel/polyfill');
+require('@babel/register')({
+  presets: ['@babel/preset-env'],
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-class-properties',
+  ],
+});
+
+console.log('Starting App...');
+
+module.exports = require('../app/server.js');
